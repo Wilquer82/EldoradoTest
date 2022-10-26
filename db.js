f886db3b989460fac8a5fca11ca6513907181e05
wilquer82 <wilquer.fig@gmail.com>
2022-10-26 08:34:53 -0300

    Db and Server OK

--- db.js
+++ db.js
@@ -1,22 +1,32 @@
-// async function connect() {
   const mysql = require('mysql2/promise');
-  async function connect(){
-    if(global.connection && global.connection.state !== 'disconnected')
-        return global.connection;
-    try {
-      const connection = await mysql.createConnection({
-        host: 'localhost',
-        port: 33060,
-        user: 'root',
-        password: 'root',
-        database: 'EldoradoTest',
-      });
-      global.connection = connection;
-      console.log("Conectou Sql");
-    } catch {
-      console.log(error);
-    }
-    return connection;
+  
+async function connect() {
+  if(global.connection && global.connection.state !== 'disconnected')
+      return global.connection;
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'Sonicfox3',
+      database: 'EldoradoTest',
+    });
+    global.connection = connection;
+    console.log("Conectou Sql");
+  } catch {
+    console.log(error);
   }
+  return connection;
+}
 
 module.exports = connect;
+
+async function Devices(){
+  const conn = await connect();
+  const [rows] = await conn.query('SELECT * FROM Devices;');
+  console.log(rows);
+  return rows;
+}
+
+module.exports = Devices;
+
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const connect = require('./db');
+const Devices = require('./db');
 const express = require('express');
 
 const app = express();
@@ -9,10 +10,7 @@ const port = 3001;
 app.listen(port, async () => {
   console.log(`Servidor Rodando em ${port}`);
 
-  // O código abaixo é para testarmos a comunicação com o MySQL
-  connect('show tables');
-  // if (result) {
-  //   console.log('MySQL connection OK');
-  // }
-
 });
+
+connect();
+Devices();
